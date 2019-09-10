cc0da36c1009fd44ffb1ff22ab23a7467a0e99ae
ArtakKostanyan <artak101991@gmail.com>
2019-09-10 12:49:08 +0400

    Change show  action

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,80 +5,16 @@ namespace App\Http\Controllers;
 use App\Comment;
 use App\Post;
 use Illuminate\Http\Request;
-use function foo\func;
+use App\PrintCommnets;
 
 class PostController extends Controller
 {
 
-    public $parents = [];
-    public $children = [];
-
-
-     public function space($count){
-
-         for ($count; $count > 0; $count--) {
-
-                echo "!!!";
-            }
-     }
-
-     public function print_Commnet($parent,$count=0){
-
-        foreach ($parent as $k=>$v){
-
-              $this->space($count);
-              echo $v->body;
-              echo "<br>";
-
-            if (  !empty ($this->children[$v->id])){
-
-                $this->print_Commnet($this->children[$v->id],$count+1);
-            }
-        }
-    }
-
     public function index()
     {
-
-
-        $comments = Comment::all();
-
-
-        foreach ($comments as $comment) {
-
-            if ($comment->parent_id === NULL) {
-
-                $this->parents[$comment->id][] = $comment;
-
-            } else {
-
-                $this->children[$comment->parent_id][] = $comment;
-            }
-        }
-
-
-        foreach ($this->parents as $parent){
-
-            $this->print_Commnet($parent);
-        }
-
-
-
-
-
-
-
-
-         $posts =  Post::all();
-
-
+       $posts =  Post::all();
         return view('posts.index', compact('posts'));
     }
-
-
-
-
-
 
     /**
      * Show the form for creating a new resource.
@@ -113,9 +49,9 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        $post = Post::find($id);
-        return view('posts.show', compact('post'));
+        $comments=new PrintCommnets($post->comments,$post);
+        return view('posts.show',  ['post'=>$post,'comments'=>$comments]);
     }
 }
